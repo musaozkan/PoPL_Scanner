230d9e16235f719b38b0740e7cf2b80d38e5fd66
barisgirayakman <girayakman@gmail.com>
2024-04-14 00:12:50 +0300

    update

--- LexicalAnalyzer.java
+++ LexicalAnalyzer.java
@@ -40,7 +40,6 @@ class LexicalAnalyzer{
     }
 
     public static void errorExist(boolean condition){
-
         if(condition && !error_exist){
             error_exist = true;
             error_line = current_line;
@@ -76,8 +75,6 @@ class LexicalAnalyzer{
         return false;
     }
     
-
-
     // It provides the first condition of hexademical or binary notation.
     // It checks the first two elements of the token.
     public static boolean isHexorBin(String line){
@@ -85,7 +82,6 @@ class LexicalAnalyzer{
         return (line.length()-current_index-1>=1 && line.charAt(current_index)=='0' && (line.charAt(current_index+1) =='x' || line.charAt(current_index+1)=='b'));
 
     }
-
 
     // Method for boolean literals
     public static boolean booleanLiterals(String line) throws IOException{
@@ -98,20 +94,16 @@ class LexicalAnalyzer{
                 break;
             boolean_checker+=line.charAt(i);
         }  
-        
         if(boolean_checker.equals("true") || boolean_checker.equals("false")){
             handlePrints(i, boolean_checker, "BOOLEAN");
             if(current_index<line.length() && isBracketExist(line.charAt(current_index)))
                 printBracket(line.charAt(i));
             return true;
         }
-
         return false;
 
     }
 
-    
-    
     public static boolean charLiterals(String line) throws IOException{
         if(current_index>=line.length())
             return true;
@@ -119,30 +111,20 @@ class LexicalAnalyzer{
         if((int)line.charAt(current_index) ==39){
             String error  = "";
             
-            
-
-
             if(current_index+2==line.length() || (current_index+2<line.length() && (int)line.charAt(current_index+2)!=39)){
                 error = line.charAt(current_index)+""+line.charAt(current_index+1);
                 shootError(error);
                 System.exit(0);
             }
             else if(current_index+2<line.length() && (int)line.charAt(current_index+2)==39){
-                System.out.println("CHAR "+current_line+":"+(current_index+1));
-                output.write("CHAR "+current_line+":"+(current_index+1)+"\n");
-                positions.add(current_line+":"+(current_index+1));
-                token_content.add("'"+line.charAt(current_index+1)+"'"); // Reminder
-                if(line.charAt(current_index+2)=='\'' && line.charAt(current_index+1) == '\\'){
+                int prev = current_index;
+                if(line.charAt(current_index+2)=='\'' && line.charAt(current_index+1) == '\\')
                     current_index+=3;
-
-                }
                 else current_index+=2;
-
-                tokens.add("CHAR");
+                handlePrints(current_index, "'"+line.charAt(prev+1)+"'", "CHAR");
                 return true;
             }
                 
-            
             else if(current_index+2<line.length() && (int)line.charAt(current_index+2)!=39){
                 error = line.charAt(current_index)+""+line.charAt(current_index+1);
                 shootError(error);
@@ -236,8 +218,6 @@ class LexicalAnalyzer{
     
     }
 
-
-
     // Function for number literals.
     public static boolean numberLiterals(String line) throws IOException{
 
@@ -252,7 +232,6 @@ class LexicalAnalyzer{
         // It includes hexadecimal literal and binary literal in
         if(isHexorBin(line)){
             error = "0";
-            
             if(line.length()-current_index-1==1){
                 error_position = current_index+1;
                 error_line = current_line;
@@ -260,7 +239,6 @@ class LexicalAnalyzer{
                 error += line.charAt(current_index)+line.charAt(current_index+1);
             }
             else{
-                
                 // Before iteration, index of the first element of token and the line it's located is conserved.
                 error_position = current_index+1;
                 error_line = current_line;
@@ -273,7 +251,6 @@ class LexicalAnalyzer{
                     isHex = false;
                     error+="b";
                 }
-
                 // Checks along the token
                 for(i = current_index+2;i<line.length() && line.charAt(i)!=' ';i++){
                     
@@ -281,13 +258,9 @@ class LexicalAnalyzer{
                         break;
                     // Adjusting the error variable even if there is no error exists.
                     error+=line.charAt(i);
-                    
-
                     // If input isn't convenient with hexadecimal literal, error happens.
                     if(isHex && !((line.charAt(i)>='A' && line.charAt(i)<='F') || (line.charAt(i)>='0' && line.charAt(i)<='9') || (line.charAt(i)>='a' && line.charAt(i)<='f')))
                         error_exist = true;
-                       
-
                     // If input isn't convenient with binary literal, error happens.
                     else if( !isHex && line.charAt(i)!='0' && line.charAt(i)!='1'){
                         error_exist = true;
@@ -299,7 +272,6 @@ class LexicalAnalyzer{
                 shootError(error);
                 System.exit(0);
             }
-
             // Otherwise, line and position that token is been is displayed on the console.
             else{
                 handlePrints(i, error, "NUMBER");
@@ -313,8 +285,6 @@ class LexicalAnalyzer{
         // Write a distinct case for a situation where first element of token is E or e !
         // It includes number literal and floating point literal in one.
         else if(line.charAt(current_index) == '.' || line.charAt(current_index)=='+' || line.charAt(current_index)=='-' || (line.charAt(current_index)>='0' && line.charAt(current_index)<='9')){
-
-           
             error+=line.charAt(current_index);
             error_position = current_index+1;
             error_line = current_line;
@@ -326,11 +296,9 @@ class LexicalAnalyzer{
                 isFloatFirst = true;
             for(i = current_index+1; i<line.length() && line.charAt(i)!=' ';i++){
                 
-
                 if(isBracketExist(line.charAt(i))){
                     break;
                 }
-                
                 // If symbol is out of the symbols given in the condition below, error occurs.
                 errorExist(line.charAt(i) != 'E' && line.charAt(i) !='e' && line.charAt(i)!='+' && line.charAt(i) !='-' && line.charAt(i)!='.' && (line.charAt(i)<'0' || line.charAt(i)>'9'));    
                 // If there is more than one exponential symbol in the token, error occurs.
@@ -345,18 +313,13 @@ class LexicalAnalyzer{
                 // It arranges boolean expression if element is exponential.
                 if(line.charAt(i) =='E' || line.charAt(i)=='e')
                     exponential_used = true;
-
-
                 // Even if there is no error, token's elements are held as a string representation.
                 error += line.charAt(i);
-
                 // If dot sign exist in the token, program will iterate according to floating point literal.
                 if(line.charAt(i)=='.')
                     isFloatFirst = true;
 
             }
-
-            
             int last_element = i-1;
             if(line.charAt(last_element) =='+' || line.charAt(last_element)=='-' || line.charAt(last_element)=='E' || line.charAt(last_element)=='.' || line.charAt(last_element)=='e')
                 error_exist = true;
@@ -366,7 +329,6 @@ class LexicalAnalyzer{
                 shootError(error);
                 System.exit(0);
             }
-
             // Otherwise, line and position that token is been is displayed on the console.
             else{
                 handlePrints(i, error, "NUMBER");
@@ -374,22 +336,15 @@ class LexicalAnalyzer{
                     printBracket(line.charAt(current_index));
                 return true;
             }
-
         }
-
         return false;
-        
         // It checks the conditions according to decimal integer and floating point notation.
-
-        
     }
 
     public static void printArray(){
         for(int i = 0;i<tokens.size();i++)
             System.out.println(tokens.get(i));
-
     }
-
     public static void main(String [] args) throws IOException{
         
 
@@ -407,15 +362,9 @@ class LexicalAnalyzer{
             boolean isStringComplete = true;
             // Iteration through lines
             while(input.hasNextLine()){
-
                 String line = input.nextLine();
-                
                 // It iterates through the line.
-
-                
-                while(current_index<line.length()){
-                    
-                    
+                while(current_index<line.length()){    
                     if(string_literal && current_index+1<line.length() && line.charAt(current_index)=='\\' && line.charAt(current_index+1) =='\"'){
                         error_for_string+=line.charAt(current_index);
                         current_index+=2;
@@ -429,13 +378,10 @@ class LexicalAnalyzer{
                         tokens.add("STRING");
                         positions.add(string_literal_line+":"+(string_literal_index+1));
                         token_content.add(error_for_string+"\"");
-
                         error_for_string ="";
                         current_index++;
-                        
                         if(current_index>line.length()-1)
                             break;
-                        
                     }
                     else if(string_literal)
                         error_for_string+=line.charAt(current_index);
@@ -455,38 +401,26 @@ class LexicalAnalyzer{
                     
                     boolean isFound = false;
                     // As long as there is no space, other circumstances are considered.
-
-                    
                     if(!string_literal && line.charAt(current_index)!=' '){
-
-
                         if((checkIdentifierFirstElement(line.charAt(current_index)) && current_index==line.length()-1) || ((current_index<line.length()-1 && line.charAt(current_index+1)==' ') &&
                         (line.charAt(current_index)=='+' || line.charAt(current_index)=='-' || line.charAt(current_index)=='.'))){
                             System.out.println("IDENTIFIER "+current_line+":"+(current_index+1));
                             tokens.add("IDENTIFIER");
                             positions.add(current_line+":"+(current_index+1));
-                            
                             token_content.add(""+line.charAt(current_index));
                             output.write("IDENTIFIER "+current_line+":"+(current_index+1)+"\n");
                             isFound = true;
                         }
-                        
                         else{
-
                             isFound = numberLiterals(line);
                         } 
-                        
                         if(!isFound)
                             isFound = keywordLiteral(line);
-
                         if(!isFound)
                             isFound = booleanLiterals(line);
-
-                        
                         if(!isFound){
                             isFound = identifierLiteral(line);
                         }
-
                         if(!isFound)
                             isFound = printBracket(line.charAt(current_index));
 
@@ -503,7 +437,6 @@ class LexicalAnalyzer{
                 current_index = 0;
             }
             if(string_literal){
-                
                 System.out.println("LEXICAL ERROR "+"["+error_line+":"+error_position+"]:"+" Invalid token "+ "'"+error_for_string+"'");
                 output.write("LEXICAL ERROR "+"["+error_line+":"+error_position+"]:"+" Invalid token "+ "'"+error_for_string+"'");
                 output.flush();
